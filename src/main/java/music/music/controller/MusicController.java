e9bab917538b7e39803adf2c40f763c850ec3a27
門野義弘 <yoshihiro@monnoyoshihironoMacBook-Air.local>
2019-04-27 12:41:17 +0900

    とりあえずlocalhost動いた

--- src/main/java/music/music/controller/MusicController.java
+++ src/main/java/music/music/controller/MusicController.java
@@ -1,4 +1,17 @@
 package music.music.controller;
 
+import music.music.service.MusicService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+import java.util.List;
+
+@RestController
 public class MusicController {
+    @RequestMapping(value = "/music", method = RequestMethod.GET)
+    public List<Map<String, Object>> index(){
+        return MusicService.musicList();
+    }
 }
--- src/main/java/music/music/service/MusicService.java
+++ src/main/java/music/music/service/MusicService.java
@@ -1,4 +1,26 @@
 package music.music.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.List;
+
+@Service
 public class MusicService {
+    @Autowired
+    private static JdbcTemplate jdbcTemplate;
+
+    public MusicService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public static List<Map<String, Object>> musicList(){
+        List<Map<String, Object>> musicList = jdbcTemplate.queryForList("SELECT * FROM music");
+        musicList.forEach(System.out::println);
+        return musicList;
+
+    }
 }
